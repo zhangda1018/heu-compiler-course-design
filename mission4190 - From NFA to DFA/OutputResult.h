421d5b40e3969e765f3f35fa20fb8c90f65f708b
zhangda1018 <66893156+zhangda1018@users.noreply.github.com>
2022-04-04 19:16:50 +0800

    Add files via upload

---
+++ mission4190 - From NFA to DFA/OutputResult.h
@@ -0,0 +1,11 @@
+#ifndef OUTPUTRESULT_H_
+#define OUTPUTRESULT_H_
+
+#include "NFAToDFA.h"
+
+void OutputResult(DFA* dfa);
+void FreeNFA();
+void FreeDFA(DFA* dfa);
+
+
+#endif /* OUTPUTRESULT_H_ */
